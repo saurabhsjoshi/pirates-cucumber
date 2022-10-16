f490eb8c6a3115362b4765150f23e724752b27fa
Saurabh Joshi <saurabhsjoshi94@gmail.com>
2022-10-16 11:23:10 -0400

    [CODE] Added logger class to log data from test cases to a file

---
+++ cucumber/src/test/java/org/joshi/cucumber/Logger.java
@@ -0,0 +1,62 @@
+package org.joshi.cucumber;
+
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * Utility class that writes content to file for multiplayer acceptance tests.
+ */
+public class Logger implements Runnable {
+    private final BlockingQueue<String> queue = new ArrayBlockingQueue<>(200);
+
+    private volatile boolean stopped = false;
+
+    private final String filename;
+
+    public Logger(String filename) {
+        this.filename = filename;
+    }
+
+    @Override
+    public void run() {
+        BufferedWriter writer;
+
+        try {
+            writer = new BufferedWriter(new FileWriter(filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        while (!stopped) {
+            try {
+                var line = queue.take();
+                System.out.println(line);
+                writer.write(line);
+                writer.newLine();
+                writer.flush();
+            } catch (InterruptedException | IOException ignore) {
+                break;
+            }
+        }
+    }
+
+    public void push(String line) {
+        if (line == null || line.isBlank()) {
+            return;
+        }
+
+        try {
+            queue.put(line);
+        } catch (InterruptedException ignore) {
+        }
+    }
+
+    public void stop() {
+        stopped = true;
+    }
+}
